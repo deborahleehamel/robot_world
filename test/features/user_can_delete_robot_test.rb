ef9b6b9b95e99158da5b3d0506ac0f0bb7f9182c
Deborah Hamel <deborahleehamel@gmail.com>
2016-03-25 09:43:12 -0600

    Last commit before adding id.

--- test/features/user_can_delete_robot_test.rb
+++ test/features/user_can_delete_robot_test.rb
@@ -4,20 +4,10 @@ class UserCanDeleteRobotTest < Minitest::Test
   include TestHelpers
   include Capybara::DSL
 
-  def create_robot
-     data = {
-       name: "some name",
-       city: "some city"
-     }
-
-     robot_world.create(data)
-     @robot = robot_world.all.last
-   end
-
   def test_robot_is_deleted
     data = {
       name: "Betty",
-      city: "Atlanta"
+      city: "Atlanta",
       :state       => "Georgia",
       :birthdate   => "06-20-12",
       :date_hired  => "05-01-14",
@@ -25,11 +15,6 @@ class UserCanDeleteRobotTest < Minitest::Test
 
       robot_world.create(data)
       robot = robot_world.all.first
-
-      visit '/robots'
-
-      click_link
-
 
 
     visit '/robots'
