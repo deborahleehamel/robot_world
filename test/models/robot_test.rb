fa2ae2b1dc011e15f8fb129b6b827057c967d6c4
Deborah Hamel <deborahleehamel@gmail.com>
2016-03-26 15:30:17 -0600

    Fixed robot_test now passing.

--- test/models/robot_test.rb
+++ test/models/robot_test.rb
@@ -3,24 +3,26 @@ require_relative '../test_helper'
 class RobotTest < Minitest::Test
 
   def test_robot_assigns_attributes_correctly
-    robot = Robot.new({"id"         => 1,
-                       "name"       => "Ioanna",
-                       "city"       => "WheatRidge",
-                       "state"      => "Colorado",
-                       "avatar"     => "7788",
-                       "birthdate"  => "1982-05-28",
-                       "date hired" => "2014-06-08",
-                       "department" => "Generosity Engineering"})
+
+    data = {:id         => 1,
+            :name       => "Maureen",
+            :city       => "WheatRidge",
+            :state      => "Colorado",
+            :avatar     => "7788",
+            :birthdate  => "06-28-1999",
+            :date_hired => "06-08-2014",
+            :department => "Generosity Engineering"}
+
+    robot = Robot.new(data)
 
     assert_equal 1, robot.id
-    assert_equal "Ioanna", robot.name
+    assert_equal "Maureen", robot.name
     assert_equal "WheatRidge", robot.city
     assert_equal "Colorado", robot.state
     assert_equal "7788", robot.avatar
-    assert_equal "1982-05-28", robot.birthdate
-    assert_equal "2014-06-08", robot.date_hired
+    assert_equal "06-28-1999", robot.birthdate
+    assert_equal "06-08-2014", robot.date_hired
     assert_equal "Generosity Engineering", robot.department
   end
-
 
 end
